320a9d5d26c4ad00d0725af04917da0f4714a8f3
Andry Ariadi <andryariadi23@gmail.com>
2024-08-12 14:01:24 +0700

    styling chatlist component

--- src/components/list/chatLists/ChatLists.jsx
+++ src/components/list/chatLists/ChatLists.jsx
@@ -1,6 +1,35 @@
+import { BiSearchAlt } from "react-icons/bi";
 import "./chatLists.css";
+import { LuMinus, LuPlus } from "react-icons/lu";
+import { useState } from "react";
 const ChatLists = () => {
-  return <div className="chatLists">ChatLists</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="chatLists">
+      <div className="search">
+        <div className="searchBar">
+          <BiSearchAlt size={24} />
+          <input type="text" placeholder="Search..." />
+        </div>
+        <div className="icon" onClick={() => setOpen(!open)}>
+          {open ? <LuMinus size={24} /> : <LuPlus size={24} />}
+        </div>
+      </div>
+
+      <div className="userChatContainer">
+        {[...Array(10)].map((_, i) => (
+          <div className="userChat" key={i}>
+            <img src="./noAvatar.png" alt="User" />
+            <div className="text">
+              <span>Tasmiah</span>
+              <p>Hello bre...</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ChatLists;
