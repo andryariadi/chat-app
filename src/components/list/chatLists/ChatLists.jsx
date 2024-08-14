8a2ccd22cddd0a046f387a193b888fb700b429eb
Andry Ariadi <andryariadi23@gmail.com>
2024-08-14 16:20:01 +0700

    fetch chats list in chat comp

--- src/components/list/chatLists/ChatLists.jsx
+++ src/components/list/chatLists/ChatLists.jsx
@@ -1,10 +1,43 @@
 import { BiSearchAlt } from "react-icons/bi";
 import "./chatLists.css";
 import { LuMinus, LuPlus } from "react-icons/lu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddUser from "./addUser/AddUser";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../../../libs/firebase";
+import { useUserStore } from "../../../libs/userStore";
 const ChatLists = () => {
   const [open, setOpen] = useState(false);
+  const [chats, setChats] = useState([]);
+
+  const { currentUser } = useUserStore();
+
+  useEffect(() => {
+    if (!currentUser.id) return;
+
+    const unSub = onSnapshot(doc(db, "userchats", currentUser.id), async (res) => {
+      console.log("Current data: ", res.data());
+
+      const items = res.data()?.chats || [];
+
+      const promises = items.map(async (item) => {
+        const userDocRef = doc(db, "users", item.receiverId);
+        const userDocSnap = await getDoc(userDocRef);
+
+        const user = userDocSnap.data();
+
+        return { ...item, user };
+      });
+
+      const chatData = await Promise.all(promises);
+
+      setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+    });
+
+    return () => unSub();
+  }, [currentUser.id]);
+
+  console.log(chats);
 
   return (
     <div className="chatLists">
@@ -19,15 +52,19 @@ const ChatLists = () => {
       </div>
 
       <div className="userChatContainer">
-        {[...Array(10)].map((_, i) => (
-          <div className="userChat" key={i}>
-            <img src="https://i.pinimg.com/236x/a5/7a/bc/a57abc03ca6359ff7b15224fa525a96a.jpg" alt="User" />
-            <div className="text">
-              <span>Tasmiah</span>
-              <p>Hello bre...</p>
+        {chats.length > 0 ? (
+          chats.map((chat) => (
+            <div className="userChat" key={chat.id}>
+              <img src="https://i.pinimg.com/236x/a5/7a/bc/a57abc03ca6359ff7b15224fa525a96a.jpg" alt="User" />
+              <div className="text">
+                <span>Tasmiah</span>
+                <p>Hello bre...</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <div className="noChat">No chat available</div>
+        )}
       </div>
       {open && <AddUser />}
     </div>
