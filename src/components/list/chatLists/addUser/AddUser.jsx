4ed6892d3d1eb37666e7fd3317cc34f9231dd3df
Andry Ariadi <andryariadi23@gmail.com>
2024-08-14 16:42:56 +0700

    handle search user

--- src/components/list/chatLists/addUser/AddUser.jsx
+++ src/components/list/chatLists/addUser/AddUser.jsx
@@ -1,22 +1,51 @@
 import { TiUserAdd } from "react-icons/ti";
 import "./addUser.css";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../../../libs/firebase";
+import { useState } from "react";
 
 const AddUser = () => {
+  const [user, setUser] = useState(null);
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+
+    try {
+      const userRef = collection(db, "users");
+
+      const q = query(userRef, where("username", "==", username));
+
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        setUser(querySnapshot.docs[0].data());
+      }
+
+      console.log(querySnapshot.docs[0].data());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="addUser">
-      <form action="">
+      <form onSubmit={handleSearch}>
         <input type="text" placeholder="Username" name="username" />
         <button>Search</button>
       </form>
-      <div className="user">
-        <div className="detailUser">
-          <img src="https://i.pinimg.com/236x/a5/7a/bc/a57abc03ca6359ff7b15224fa525a96a.jpg" alt="User" />
-          <span>Alvine</span>
+      {user && (
+        <div className="user">
+          <div className="detailUser">
+            <img src={user.avatar || "/noAvatar.png"} alt="User" />
+            <span>{user.username}</span>
+          </div>
+          <button>
+            <TiUserAdd size={24} className="icon" />
+          </button>
         </div>
-        <button>
-          <TiUserAdd size={24} className="icon" />
-        </button>
-      </div>
+      )}
     </div>
   );
 };
